5865d732448c899aca1f85ab7d38c5e727be9464
Serena <23511780+SploxFox@users.noreply.github.com>
2023-11-10 16:25:02 -0500

    Wrote the thing

---
+++ src/index.tsx
@@ -0,0 +1,28 @@
+import { Deck, DeckProps } from '@deck.gl/core/typed';
+import { JSX, createEffect, onMount, splitProps } from 'solid-js';
+
+export type SolidDeckProps = Omit<DeckProps, 'container' | 'canvas' | 'style'> & {
+    width?: string;
+    height?: string;
+    style?: JSX.CSSProperties;
+};
+
+export const DeckGL = (props: SolidDeckProps) => {
+    let canvas: HTMLCanvasElement;
+    let deck: Deck;
+
+    const [canvasProps, deckProps] = splitProps(props, ['width', 'height', 'style']);
+
+    onMount(() => {
+        deck = new Deck({
+            ...deckProps,
+            canvas
+        });
+    });
+
+    createEffect(() => {
+        deck.setProps(deckProps);
+    });
+
+    return <canvas ref={canvas!} {...canvasProps} />;
+}
